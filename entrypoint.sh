78d4226ba32c4df93e75db5d3f91de2a498f5362
Sohaib kamran <sohaibkamran.1122@gmail.com>
2026-05-21 12:47:28 +0500

    fix: aggressively remove all mpm configurations including dangling .conf files to prevent syntax errors

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Strictly enforce only mpm_prefork is loaded
-rm -f /etc/apache2/mods-enabled/mpm_*.load
+rm -f /etc/apache2/mods-enabled/mpm_*
 a2enmod mpm_prefork
 
 # Inject Railway's dynamic PORT into Apache configuration
